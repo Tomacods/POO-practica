e7ca2b250fb8532f91e246d14cdf1966d15315d8
federxldan <federoldan229@gmail.com>
2024-08-08 12:23:26 -0300

    AAAAAAAAAAA
    :3

---
+++ ejercicio6tp1/ejercicio6.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class ejercicio6{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese su nombre: ");
+        String nombre = scanner.nextLine();
+        System.out.println("Bienvenida/o, " + nombre);
+    }
+}
