5c60b9d3eaf1d213c27fa70efa30fd5d9ff2055f
Tomás <Tomasds15@gmail.com>
2024-08-10 18:12:21 -0300

    java

--- TP1 python/Cesar/tp1/punto4/main.py
+++
@@ -1,8 +0,0 @@
-
-from Libro import Libro
-from Categoria import Categoria
-##self.__img, self.__titulo,self.__autor,self.__calificacion,self.__precio)
-articulo = Libro("imagen","Libro Python","Messi Lionel","3 Estrellas","U$D 10")
-Categoria = Categoria("Mas vendidos")
-Categoria.agregar_articulo(articulo)
-Categoria.imprimir()
