22f2f30d8664895244fcf9278d56cf784ecbd1a8
Tomás <Tomasds15@gmail.com>
2024-08-12 10:12:59 -0300

    Update Ticket.py

--- TP1/TP1 python/python fede/ejercicio 2/Ticket.py
+++ TP1/TP1 python/python fede/ejercicio 2/Ticket.py
@@ -1,22 +1,22 @@
-from datetime import datetime
+from datetime import datetime #importo la libreria datetime
 
 class Ticket:
     
-    def __init__(self, nroVenta):
+    def __init__(self, nroVenta): #constructor de la clase
         self.nroVenta = nroVenta
         self.fecha = datetime.now()
         self.productos = []
         
     def agregarProducto(self, producto):
-        self.productos.append(producto)
+        self.productos.append(producto) #el append agrega un elemento a la lista 
     
-    def calcularTotal(self):
+    def calcularTotal(self): #metodo para calcular el total de la venta
         total = 0.0
         for produ in self.productos:
             total += produ.getPrecio()
         return total
     
-    def imprimir(self):
+    def imprimir(self): #metodo para imprimir el ticket
         print("Nro de venta: {}          Fecha: {}".format(self.nroVenta, self.fecha))
         print("Articulo       Dosis          Precio")
         for produ in self.productos:
