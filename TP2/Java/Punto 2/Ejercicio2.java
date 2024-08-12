a0abd0c70db9ad1a4da9ad1366916c93420bc83b
federxldan <federoldan229@gmail.com>
2024-08-12 20:52:41 -0300

    A

--- TP2/Java/Punto 2/Ejercicio2.java
+++ TP2/Java/Punto 2/Ejercicio2.java
@@ -1,7 +1,7 @@
 public class Ejercicio2 {
     
     public static void main(String[] args) throws Exception {
-        Cancion cancion = new Cancion();
-        
+        Cancion cancion = new Cancion("Un velero llamado libertad", "Jose Luis Perales", 222.0);
+        cancion.imprimir();
     }
 }
