cb5ae12e3519e99f2bdd9f2cc73cd1dc09a04688
federxldan <federoldan229@gmail.com>
2024-08-12 20:17:04 -0300

    A

---
+++ TP2/Java/Punto 2/Cancion.java
@@ -0,0 +1,7 @@
+public class Cancion {
+    
+    private String nombre;
+    private String autor;
+    private Double duracion;
+
+}
--- TP2/Python/Ejercicio2.py
+++ TP2/Python/Ejercicio2.py
@@ -3,3 +3,4 @@ class Cancion:
     def __init__(self, nombre, autor, duracion):
         self.nombre = nombre
         self.autor = autor
+        self
