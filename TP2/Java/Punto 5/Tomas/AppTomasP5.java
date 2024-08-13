d5e2164a1d987aad0ce18af4c45c93757fc9c994
Tomás <Tomasds15@gmail.com>
2024-08-13 15:21:47 -0300

    Tomas

---
+++ TP2/Java/Punto 5/Tomas/AppTomasP5.java
@@ -0,0 +1,20 @@
+import java.time.LocalDate; 
+
+
+
+public class AppTomasP5 {
+    
+    public static void main(String[] args) {
+    
+    PersonaTomas persona1 = new PersonaTomas("Tomas", "Gonzalez", LocalDate.of(1999, 10, 10));
+    PersonaTomas persona2 = new PersonaTomas("Juan", "Perez", LocalDate.of(1998, 11, 11));
+    PersonaTomas persona3 = new PersonaTomas("Pedro", "Gomez", LocalDate.of(1997, 12, 12));
+
+
+    System.out.println(persona1.toString());
+    System.out.println(persona2.toString());
+    System.out.println(persona3.toString());
+
+    }
+}
+
---
+++ TP2/Java/Punto 5/Tomas/PersonaTomas.java
@@ -0,0 +1,35 @@
+import java.time.LocalDate;
+
+
+public class PersonaTomas {
+    private String nombre;
+    private String apellido;
+    private LocalDate  fechaNacimiento;
+
+    public PersonaTomas(String nombre, String apellido, LocalDate fechaNacimiento) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.fechaNacimiento = fechaNacimiento;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public String getApellido() {
+        return apellido;
+    }
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+    public void setFechaNacimiento(LocalDate fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+    public String toString() {
+        return "Nombre: " + nombre + ", Apellido: " + apellido + ", Fecha de Nacimiento: " + fechaNacimiento;
+    }
+}
