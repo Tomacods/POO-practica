a1164b539df8b1e41f4ef5cffb67fdc84442e9fa
federxldan <federoldan229@gmail.com>
2024-08-14 17:37:04 -0300

    Update Fede_P9_Familia.java

--- TP2/Java/Punto 9/Fede/Fede_P9_Familia.java
+++ TP2/Java/Punto 9/Fede/Fede_P9_Familia.java
@@ -4,15 +4,23 @@ public class Fede_P9_Familia {
 
     private String flia = "";
     private Integer cantidad;
+    private Integer total;
     private ArrayList<Fede_P9_Familia> personas = new ArrayList<>();
 
     public Fede_P9_Familia(String flia){
         this.flia = flia;
     }
 
+    public void cantidad(){
+        System.out.println("Esta familia contiene " + personas.size() + " integrantes.");
+    }
+
+    /*public void promedioEdad(){
+
+    } */
+
     public void addFamilia(Fede_P9_Familia persona, Integer cantidad){
         this.personas.add(persona);
-        this.cantidad += 1;
     }
 
     public Integer getCantidad(){
