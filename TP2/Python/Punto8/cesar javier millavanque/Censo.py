d486b80d97df30bf458c9ea36994725aa04c3527
Cesarmi99 <cesarmiuni@gmail.com>
2024-08-14 19:11:29 -0300

    mbappeeee

---
+++ TP2/Python/Punto8/cesar javier millavanque/Censo.py
@@ -0,0 +1,30 @@
+class Censo:
+    def __init__(self,comuna,familia):
+        self.__comuna = comuna
+        self.__familia = familia
+        self.__Persona = []
+
+    @property
+    def comuna(self):
+        return self.__comuna
+
+    @comuna.setter
+    def comuna(self,comuna):
+        self.__comuna = comuna
+    @property
+    def familia(self):
+        return self.__familia
+
+    @familia.setter
+    def familia(self,familia):
+        self.__familia = familia
+    
+    def agregar_persona (self,Persona):
+        self.__Persona.append(Persona)
+    def imprimir(self):
+        print("Familia: ",self.__familia)
+        print("Comuna: ",self.__comuna)
+        print("Integrantes:")
+        for pepe in self.__Persona:
+            pepe.Imprimir()
+
