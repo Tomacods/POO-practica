797eb684f42be2b3651a04a20115323d751ea50f
Tomás <Tomasds15@gmail.com>
2024-08-14 21:29:22 -0300

    Update App11.java

--- TP2/Java/Punto 11/Tomas11/src/App11.java
+++ TP2/Java/Punto 11/Tomas11/src/App11.java
@@ -1,9 +1,13 @@
 import java.time.LocalDate;
+import java.util.Scanner;
 
 
 
 public class App11 {
     public static void main(String[] args) throws Exception {
+    
+    Scanner sc = new Scanner(System.in);
+    
 
     PersonaTomas persona = new PersonaTomas("Tomas", "Da silva", LocalDate.of(1999, 10, 10), "Masculino", true);    
     PersonaTomas persona2 = new PersonaTomas("Martina", "Da silva", LocalDate.of(1999, 10, 10), "Femenino", false);     
@@ -21,5 +25,46 @@ public class App11 {
 
     empresa.Imprimir();
     empresa.ContarEmpleados();
+    
+    while (true) { 
+        System.out.println("desea agregar un empleado? si/no");
+        String respuesta = sc.nextLine();
+        if (respuesta.equals("si")) {
+            System.out.println("Ingrese el nombre del empleado");
+            String nombre = sc.nextLine();
+
+            System.out.println("Ingrese el apellido del empleado");
+            String apellido = sc.nextLine();
+
+            System.out.println("Ingrese la fecha de nacimiento del empleado en formato yyyy-mm-dd");
+            String fechaNacimientoCade = sc.nextLine();
+            LocalDate fechaNacimiento = LocalDate.parse(fechaNacimientoCade);
+
+
+            System.out.println("Ingrese el sexo del empleado");
+            String sexo = sc.nextLine();
+
+            System.out.println("Ingrese si el empleado trabaja o no");
+            String estudia_Trabaja = sc.nextLine();
+
+            boolean estudia_TrabajaBoolean = false;
+            if (estudia_Trabaja.equals("si")) {
+                estudia_TrabajaBoolean = true;
+            }
+            PersonaTomas NuevoEmpleado = new PersonaTomas(nombre, apellido, fechaNacimiento, sexo, estudia_TrabajaBoolean);
+            empresa.addEmpleado(NuevoEmpleado);
+            empresa.Imprimir();
+            empresa.ContarEmpleados();
+        } else {
+            System.out.println("esta seguro que desea salir? si/no");
+            String respuesta2 = sc.nextLine();
+            if (respuesta2.equals("si")) {
+            break;
+        }
+    }
+
+
+    }
+    sc.close();
     }
 }
