d84ca39881540ebbf2ddf53138da8b7219edd7a0
federxldan <federoldan229@gmail.com>
2024-08-15 16:30:41 -0300

    A?

--- TP2/Python/Punto2/Fede3p/App 2.py
+++ TP2/Python/Punto2/Fede3p/App 2.py
@@ -1,5 +1,5 @@
 import FedeCancion
 
-cancion = FedeCancion("Un velorio llamado libertad", "Jose Luis Perales", 222.0)
+cancion = FedeCancion.FedeCancion("Un velorio llamado libertad", "Jose Luis Perales", 222.0)
 
 FedeCancion.imprimir(cancion)
