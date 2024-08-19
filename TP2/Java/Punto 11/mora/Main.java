11ce2447be05b3121a4fde064507f3c992990d8e
Mora <mor4lina2003@gmail.com>
2024-08-19 00:36:12 -0300

    mora

---
+++ TP2/Java/Punto 11/mora/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Empresa empresaa = new Empresa("TABLADA", "CODIGO 6569");
+
+        Persona empleado1 = new Persona("Mora", 21, "F", "Oficina");
+        Persona empleado2 = new Persona("Maria", 26, "F", "Oficina");
+        Persona empleado3 = new Persona("Leandro", 34, "M", "Recursos Humanos");
+        Persona empleado4 = new Persona("Victor", 43, "M", "Oficina");
+        Persona empleado5 = new Persona("Rocio", 24, "F", "Atención al cliente");
+
+        empresaa.addPersona(empleado1);
+        empresaa.addPersona(empleado2);
+        empresaa.addPersona(empleado3);
+        empresaa.addPersona(empleado4);
+        empresaa.addPersona(empleado5);
+
+        System.out.println(empresaa);
+        
+        for (Persona persona : empresaa.getPersonas()){
+            System.out.println(persona);
+            System.out.println();
+        }
+    }
+}
