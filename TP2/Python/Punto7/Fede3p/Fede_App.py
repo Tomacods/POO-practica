095135141c815194af00e2f531c264381237f24d
federxldan <federoldan229@gmail.com>
2024-08-21 19:00:40 -0300

    A

---
+++ TP2/Python/Punto7/Fede3p/Fede_App.py
@@ -0,0 +1,12 @@
+from Fede_Persona7 import Fede_Persona7
+from datetime import date
+
+persona1 = Fede_Persona7("Federica", "Roldan", date(2003,9,22))
+persona2 = Fede_Persona7("Mora", "Molina", date(2003,7,25))
+persona3 = Fede_Persona7("Tomas", "Da Silva", date(2000,3,9))
+persona4 = Fede_Persona7("Cesar", "Millavanque", date(1999,7,13))
+
+print(persona1)
+print(persona2)
+print(persona3)
+print(persona4)
