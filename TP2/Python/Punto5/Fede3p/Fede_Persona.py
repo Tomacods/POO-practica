ecbb87762f77ccc800320c6f350025a4f06e6337
federxldan <federoldan229@gmail.com>
2024-08-21 18:36:36 -0300

    python m la peló

---
+++ TP2/Python/Punto5/Fede3p/Fede_Persona.py
@@ -0,0 +1,35 @@
+from datetime import datetime
+
+class Fede_Persona:
+        
+    def __init__(self, nombre, apellido, nacimiento):
+        self.__nombre = nombre
+        self.__apellido = apellido
+        self.__nacimiento = nacimiento
+        
+    @property
+    def nombre(self):
+        return self.__nombre
+    
+    @nombre.setter
+    def set_nombre(self, nombre):
+        self.__nombre = nombre
+        
+    @property
+    def apellido(self):
+        return self.__apellido
+    
+    @apellido.setter
+    def set_apellido(self, apellido):
+        self.__apellido = apellido
+    
+    @property
+    def nacimiento(self):
+        return self.__nacimiento
+    
+    @nacimiento.setter
+    def set_nacimiento(self, nacimiento):
+        self.__nacimiento = nacimiento
+        
+    def __str__(self):
+        return f'{"Nombre:"}{self.nombre}{" -  Apellido:"}{self.apellido}{" -  Fecha de nacimiento:"}{self.nacimiento}'
