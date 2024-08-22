b0071ad74fac51d5bdf37efb6fecadc890b73bda
Tomás <Tomasds15@gmail.com>
2024-08-22 00:59:07 -0300

    Tomas

--- TP3/JAVA/ejercicio 4/Tomas/HerenciaPersonaje/src/Juego.java
+++ TP3/JAVA/ejercicio 4/Tomas/HerenciaPersonaje/src/Juego.java
@@ -1,18 +1,48 @@
+import java.util.Random;
+
 public class Juego {
     public static void main(String[] args) throws Exception {
-        
         Personaje cesar = new PjCesar("Cesar");
         Personaje mora = new PjMora("Mora");
 
-    for (int i = 0; i < 9; i++) {
-            cesar.recibirDano(mora.atacar());
-            mora.recibirDano(cesar.atacar());
-        } 
-        System.out.println("Vida de Cesar: " + cesar.getVida());
-        System.out.println("Vida de Mora: " + mora.getVida());
-        
-        
-        
+        for (int i = 0; i < 3; i++) {
+            try {
+                realizarCombate(cesar, mora);
+            } catch (VidaNegativaException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
+    public static void realizarCombate(Personaje p1, Personaje p2) throws VidaNegativaException {
+        Random random = new Random();
+        boolean turnoP1 = random.nextBoolean();
+
+        while (p1.getVida() > 0 && p2.getVida() > 0) {
+            if (turnoP1) {
+                p2.recibirDano(p1.atacar());
+            } else {
+                p1.recibirDano(p2.atacar());
+            }
+            turnoP1 = !turnoP1;
+
+            if (p1.getVida() < 0) {
+                p1.setVida(0);
+                throw new VidaNegativaException("La vida de " + p1.getNombre() + " ha llegado a un valor negativo. Se establece en 0.");
+            }
+            if (p2.getVida() < 0) {
+                p2.setVida(0);
+                throw new VidaNegativaException("La vida de " + p2.getNombre() + " ha llegado a un valor negativo. Se establece en 0.");
+            }
+        }
+
+        System.out.println("Vida de " + p1.getNombre() + ": " + p1.getVida());
+        System.out.println("Vida de " + p2.getNombre() + ": " + p2.getVida());
+    }
+}
+
+class VidaNegativaException extends Exception {
+    public VidaNegativaException(String message) {
+        super(message);
+    }
 }
--- TP3/JAVA/ejercicio 4/Tomas/HerenciaPersonaje/src/Personaje.java
+++ TP3/JAVA/ejercicio 4/Tomas/HerenciaPersonaje/src/Personaje.java
@@ -6,7 +6,7 @@ Cada clase debe establecer una cantidad de puntos de vida por defecto.
 En un ataque se deben realizar cierta cantidad de puntos de daño y en la defensa se
 debe reducir esa cantidad de puntos de daños. El resultado final de los puntos de ataque
 debe descontar la misma cantidad de puntos de vida del personaje que defiende.*/
-
+import java.util.Random;
 public abstract class Personaje {
     protected String nombre;
     protected int vida;
@@ -28,6 +28,9 @@ public abstract class Personaje {
     public int getVida() {
         return vida;
     }
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
@@ -42,5 +45,36 @@ public abstract class Personaje {
         if (vida < 0)
             vida = 0;
     }
+    
+    public static void realizarCombate(Personaje p1, Personaje p2) throws VidaNegativaException {
+        Random random = new Random();
+        boolean turnoP1 = random.nextBoolean();
 
+        while (p1.getVida() > 0 && p2.getVida() > 0) {
+            if (turnoP1) {
+                p2.recibirDano(p1.atacar());
+            } else {
+                p1.recibirDano(p2.atacar());
+            }
+            turnoP1 = !turnoP1;
+
+            if (p1.getVida() < 0) {
+                p1.setVida(0);
+                throw new VidaNegativaException("La vida de " + p1.getNombre() + " ha llegado a un valor negativo. Se establece en 0.");
+            }
+            if (p2.getVida() < 0) {
+                p2.setVida(0);
+                throw new VidaNegativaException("La vida de " + p2.getNombre() + " ha llegado a un valor negativo. Se establece en 0.");
+            }
+        }
+
+        System.out.println("Vida de " + p1.getNombre() + ": " + p1.getVida());
+        System.out.println("Vida de " + p2.getNombre() + ": " + p2.getVida());
+    }
 }
+    class VidaNegativaException extends Exception { // Se crea una excepción para manejar la vida negativa
+        public VidaNegativaException(String message) {
+            super(message);
+        }
+    }
+
