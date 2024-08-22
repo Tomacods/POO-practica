aeff682982e92565388553289f8093bfda71d57f
Tomás <Tomasds15@gmail.com>
2024-08-22 12:51:18 -0300

    Tomas

--- TP3/JAVA/ejercicio 4/Tomas/HerenciaPersonaje/src/Juego.java
+++ TP3/JAVA/ejercicio 4/Tomas/HerenciaPersonaje/src/Juego.java
@@ -17,7 +17,24 @@ public class Juego {
             }
             turnoP1 = !turnoP1; //cambia el turnoD
     }
-    System.out.println("Vida de " + cesar.getNombre() + ": " + cesar.getVida()); //imprime la vida de cesar al final del combate
-    System.out.println("Vida de " + mora.getNombre() + ": " + mora.getVida()); //imprime la vida de mora al final del combate
+    if (cesar.getVida() <= 0) { //si la vida de cesar es menor o igual a 0
+        cesar.setVida(0); //la vida de cesar es igual a 0
     }
+    if (mora.getVida() <= 0) { //si la vida de mora es menor o igual a 0
+        mora.setVida(0); //la vida de mora es igual a 0
+    }
+    // System.out.println("Vida de " + cesar.getNombre() + ": " + cesar.getVida()); //imprime la vida de cesar al final del combate
+    // System.out.println("Vida de " + mora.getNombre() + ": " + mora.getVida()); //imprime la vida de mora al final del combate
+    
+    //mostrar al ganador
+    if (cesar.getVida() > mora.getVida()) { //si la vida de cesar es mayor a la vida de mora
+        System.out.println("El ganador es: " + cesar.getNombre()); //imprime que el ganador es cesar
+    } else if (mora.getVida() > cesar.getVida()) { //si la vida de mora es mayor a la vida de cesar
+        System.out.println("El ganador es: " + mora.getNombre()); //imprime que el ganador es mora
+    } else { //si la vida de cesar es igual a la vida de mora
+        System.out.println("Empate"); //imprime que hay un empate
+    }
+
 }
+}
+//mas tarde implementar una exepcion para la vida negativa
--- TP3/JAVA/ejercicio 4/Tomas/HerenciaPersonaje/src/PjMora.java
+++ TP3/JAVA/ejercicio 4/Tomas/HerenciaPersonaje/src/PjMora.java
@@ -4,7 +4,7 @@ public class PjMora extends Personaje{
     private static final int VIDA_POR_DEFECTO = 100;
 
     public PjMora(String nombre, int vida) {
-        super(nombre, vida, 15, 100);
+        super(nombre, vida, 10, 100);
     }
 
     public PjMora(String nombre) {
