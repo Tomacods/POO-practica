883bec1bed17856629786814ee41efd40255bd8e
Tomás <Tomasds15@gmail.com>
2024-08-23 13:21:54 -0300

    tomas

---
+++ TP3/JAVA/ejercicio 9/Parcial2018/src/Personal.java
@@ -0,0 +1,16 @@
+public class Personal extends TarifaProveedor {
+    public Personal(int totalSMS, int totalMinutos, int totalGigas) {
+        super(totalSMS, totalMinutos, totalGigas);
+    }
+
+    @Override
+    public String getNombre() {
+        return "Personal";
+    }
+    // Personal: que tiene un 20% extra sobre los minutos de llamada y 50% sobre los GB de datos.
+    @Override
+    public double calcular(int totalSMS, int totalMinutos, int totalGigas) {
+        return super.calcularSMS(totalSMS) + calcularMinutosDeLlamada(totalMinutos) * 1.20 + calcularConsumoGB(totalGigas) * 1.50; // 20% extra sobre los minutos de llamada y 50% sobre los GB de datos
+    }
+
+}
