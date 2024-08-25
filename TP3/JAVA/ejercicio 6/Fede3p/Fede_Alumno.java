e21aaa2d4f6ee9e08fad1a5ce0d2ae05345a051f
federxldan <federoldan229@gmail.com>
2024-08-25 14:12:32 -0300

    a

--- TP3/JAVA/ejercicio 6/Fede3p/Fede_Alumno.java
+++ TP3/JAVA/ejercicio 6/Fede3p/Fede_Alumno.java
@@ -8,4 +8,21 @@ public class Fede_Alumno extends Fede_Persona {
         super(nombre, apellido);
         this.materias = new ArrayList<>();
     }
+
+    public ArrayList<String> getMaterias() {
+        return materias;
+    }
+
+    public void setMaterias(ArrayList<String> materias) {
+        this.materias = materias;
+    }
+
+    @Override
+    public String materia(){
+        String msg = "El alumno se encuentra cursando la siguiente materias \n";
+        for (String mat : this.materias){
+            msg += mat + "\n";
+        }
+        return msg;
+    }
 }
