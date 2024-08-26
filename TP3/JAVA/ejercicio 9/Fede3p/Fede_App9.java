24b00ea27c0d53eae268c204781f3d89ae305937
federxldan <federoldan229@gmail.com>
2024-08-26 20:53:00 -0300

    a

--- TP3/JAVA/ejercicio 9/Fede3p/Fede_App9.java
+++ TP3/JAVA/ejercicio 9/Fede3p/Fede_App9.java
@@ -13,11 +13,15 @@ public class Fede_App9 {
         Integer gb = scanner.nextInt();
 
         Fede_Claro claro = new Fede_Claro("Claro", sms, min, gb);
+
         Fede_Personal personal = new Fede_Personal("Personal", sms, min, gb);
+        
         Fede_Movistar movistar = new Fede_Movistar("Movistar", sms, min, gb);
 
         System.out.println("El proveedor " + claro.getNombre() + " ofrece una tarifa de $" + claro.totalTarifa());
         System.out.println("El proveedor " + personal.getNombre() + " ofrece una tarifa de $" + personal.totalTarifa());
         System.out.println("El proveedor " + movistar.getNombre() + " ofrece una tarifa de $" + movistar.totalTarifa());
+
+        scanner.close();
     }
 }
