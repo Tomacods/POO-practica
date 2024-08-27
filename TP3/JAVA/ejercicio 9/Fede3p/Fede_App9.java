0bef9737ad203be295a52b767d3b860cfaf7350b
federxldan <federoldan229@gmail.com>
2024-08-26 22:03:20 -0300

    Update Fede_App9.java

--- TP3/JAVA/ejercicio 9/Fede3p/Fede_App9.java
+++ TP3/JAVA/ejercicio 9/Fede3p/Fede_App9.java
@@ -13,9 +13,7 @@ public class Fede_App9 {
         Integer gb = scanner.nextInt();
 
         Fede_Claro claro = new Fede_Claro("Claro", sms, min, gb);
-
         Fede_Personal personal = new Fede_Personal("Personal", sms, min, gb);
-        
         Fede_Movistar movistar = new Fede_Movistar("Movistar", sms, min, gb);
 
         System.out.println("El proveedor " + claro.getNombre() + " ofrece una tarifa de $" + claro.totalTarifa());
