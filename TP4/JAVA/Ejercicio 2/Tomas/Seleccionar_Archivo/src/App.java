dfd47a67f719112fc4d165fb925ca7f82164933d
Tomás <Tomasds15@gmail.com>
2024-09-03 20:45:10 -0300

    Tomas

--- TP4/JAVA/Ejercicio 2/Tomas/Seleccionar_Archivo/src/App.java
+++ TP4/JAVA/Ejercicio 2/Tomas/Seleccionar_Archivo/src/App.java
@@ -2,13 +2,14 @@
 ubicación de destino. Luego, al presionar un botón este archivo debe almacenarse en la
 ubicación indicada.  */
 
-import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import javax.swing.*; // importo la libreria para poder usar JFrame y JButton
+import java.awt.event.ActionEvent;  // importo la libreria para poder usar ActionEvent que hace que un objeto pueda enviar eventos
+import java.awt.event.ActionListener; // importo la libreria para poder usar ActionListener que hace que un objeto pueda escuchar eventos
+import java.io.File; // importo la libreria para poder usar File
+import java.io.FileInputStream; // importo la libreria para poder usar FileInputStream que hace que se pueda leer un archivo
+import java.io.FileOutputStream; // importo la libreria para poder usar FileOutputStream que hace que se pueda escribir un archivo
+import java.io.IOException; // importo la libreria para poder usar IOException que hace que se pueda manejar excepciones
+
 
 public class App {
     public static void main(String[] args) {
@@ -61,7 +62,7 @@ public class App {
                 if (selectedFile[0] != null && destinationFile[0] != null) {
                     File destFile = new File(destinationFile[0], selectedFile[0].getName());
                     try (FileInputStream fis = new FileInputStream(selectedFile[0]);
-                         FileOutputStream fos = new FileOutputStream(destFile)) {
+                        FileOutputStream fos = new FileOutputStream(destFile)) {
                         byte[] buffer = new byte[1024];
                         int length;
                         while ((length = fis.read(buffer)) > 0) {
