4901cf3c02d62385bd8809d17d8e536738257904
Tomás <Tomasds15@gmail.com>
2024-09-04 16:15:41 -0300

    Tomas

---
+++ TP4/JAVA/Ejercicio 2/Tomas/Seleccionar_Archivo/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+public static void main(String[] args) {
+    ModeloArchivo modelo = new ModeloArchivo();
+    VistaArchivo vista = new VistaArchivo();
+    new ControladorArchivo(modelo, vista);
+}
+
+}
--- TP4/JAVA/Ejercicio 2/Tomas/Seleccionar_Archivo/src/VistaArchivo.java
+++ TP4/JAVA/Ejercicio 2/Tomas/Seleccionar_Archivo/src/VistaArchivo.java
@@ -1,4 +1,4 @@
-package Ejercicio;
+
 
 import java.awt.*;
 import java.awt.event.ActionListener;
