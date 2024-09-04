512c0aadaaf0f3622ce94bd778924bfdb002f54e
federxldan <federoldan229@gmail.com>
2024-09-04 14:46:43 -0300

    se dice gracias

--- TP3/JAVA/ejercicio 10/Fede3p/Fede_App10.java
+++ TP3/JAVA/ejercicio 10/Fede3p/Fede_App10.java
@@ -10,12 +10,45 @@ public class Fede_App10 {
 
         for (Integer i = 0; i <10; i++){
             if (random.nextInt(2) == 1){
+                // Se crea la lista de categorías aleatorias.
                 String[] categorias = {"Simple", "Semiexclusiva", "Exclusiva"};
+                // Se asigna una categoría aleatoria.
                 String categoria = categorias[random.nextInt(categorias.length)];
-                Integer horasMensual;
+                // Se generan las horas mensuales.
+                Integer horasMensual = horasTrabajadas(categoria, true, random);
+                // Se carga el personal docente
+                personal[i] = new Fede_Docente10("Nombre " + i, "Apellido " + i, i, "Sector " + i, horasMensual, categoria);
+            } else {
+                
             }
         }
     }
 
-    private static Integer horasTrabajadas;
+    private static Integer horasTrabajadas(String tipo, Boolean esDocente, Random random){
+        Integer horasRequeridas = 40;
+        Integer horasTrabaj = horasRequeridas;
+        Double probabilidadExceder = 0.0;
+
+        if (esDocente) {
+            switch (tipo) {
+                case "Simple": probabilidadExceder = 0.95;
+                case "Semiexclusiva": probabilidadExceder = 0.75;
+                case "Exclusiva": probabilidadExceder = 0.60;
+            }
+        } else {
+            switch (tipo) {
+                case "Completa": probabilidadExceder = 0.95;
+                case "Parcial": probabilidadExceder = 0.75;
+                    
+                    break;
+            
+                default:
+                    break;
+            }
+        }
+        if (random.nextDouble() <probabilidadExceder){
+            horasTrabaj += random.nextInt(21);
+        }
+        return horasTrabaj;
+    }
 }
--- TP3/JAVA/ejercicio 8/Csar/Cesar_Suplente.java
+++ TP3/JAVA/ejercicio 8/Csar/Cesar_Suplente.java
@@ -1,7 +1,11 @@
 public class Cesar_Suplente extends Cesar_Profesor{
     
-    public Cesar_Suplente(){
-
+    public Cesar_Suplente(String nombre, String apellido, Integer edad, Integer horasTrabajadas){
+        super(nombre, apellido, edad, horasTrabajadas);
     }
 
+    @Override
+    public Double getRemuneracionMensual(){
+        return 0.0;
+    }
 }
