4da642954b4b85e2a9d3827436ff3e3364a3b4c1
Cesarmi99 <cesarmiuni@gmail.com>
2024-09-08 15:50:41 -0300

    ????

--- TP4/JAVA/Ejercicio 1/Mora/b/Dialog2.java
+++ TP4/JAVA/Ejercicio 1/Mora/b/Dialog2.java
@@ -2,7 +2,7 @@ import javax.swing.JOptionPane;
 public class Dialog2  {
     public static void main(String[] args)  {
 String texto_ingresado = JOptionPane.showInputDialog("Ingrese un texto");
-JOptionPane.showMessageDialog(null, texto_ingresado, null, 3);
+JOptionPane.showMessageDialog(null, null, texto_ingresado, 3);
 
 }
 
