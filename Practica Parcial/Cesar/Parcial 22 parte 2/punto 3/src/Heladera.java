aa1daa5d219fef001695ccbcfbea348114ab732a
Cesarmi99 <cesarmiuni@gmail.com>
2024-09-08 19:19:11 -0300

    fede boluda

--- Practica Parcial/Cesar/Parcial 22 parte 2/punto 3/src/Heladera.java
+++ Practica Parcial/Cesar/Parcial 22 parte 2/punto 3/src/Heladera.java
@@ -1,9 +1,26 @@
-public class Cocina extends Electrodomestico{
+public class Heladera extends Electrodomestico{
     private Double capacidadTotal;
     private Double litro;
-    public Cocina(String modelo, String marca, String color, Double precio, Double capacidadTotal, Double litro) {
+    public Double getCapacidadTotal() {
+        return capacidadTotal;
+    }
+    public void setCapacidadTotal(Double capacidadTotal) {
+        this.capacidadTotal = capacidadTotal;
+    }
+    public Double getLitro() {
+        return litro;
+    }
+    public void setLitro(Double litro) {
+        this.litro = litro;
+    }
+    public Heladera(String modelo, String marca, String color, Double precio, Double capacidadTotal, Double litro) {
         super(modelo, marca, color, precio);
         this.capacidadTotal = capacidadTotal;
         this.litro = litro;
+    }
+    @Override
+    public String toString() {
+    
+        return super.toString()+ "Capacidad total= " + capacidadTotal + "Litro= "+ litro ;
     }
 }
