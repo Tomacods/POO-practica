40320fa57f0186a348b0abc0382fc8b40e48856c
federxldan <federoldan229@gmail.com>
2024-09-08 21:10:15 -0300

    AAAAAAAAAAAAAAAAAAAAAAA

--- Practica Parcial/Fede/Parcial 2022/Punto 3/src/App.java
+++ Practica Parcial/Fede/Parcial 2022/Punto 3/src/App.java
@@ -14,7 +14,7 @@ public class App {
             String color = colores[random.nextInt(colores.length)];
             Boolean cargaFrontal = random.nextBoolean();
             Integer capKilo = random.nextInt(31);
-            lavarropas[i] = new Lavarropa("Modelo" + i, precio, marca, color, cargaFrontal, capKilo);
+            lavarropas[i] = new Lavarropa("Modelo " + i, precio, marca, color, cargaFrontal, capKilo);
         }
         for (Integer i = 0; i < 10; i++){
             Integer precio = random.nextInt(1000000);
@@ -22,18 +22,41 @@ public class App {
             String color = colores[random.nextInt(colores.length)];
             Integer capTotal = random.nextInt(31);
             Integer litros = random.nextInt(31);
-            heladeras[i] = new Heladera("Modelo" + i, precio, marca, color, capTotal, litros);
+            heladeras[i] = new Heladera("Modelo " + i, precio, marca, color, capTotal, litros);
         }
         for (Integer i = 0; i < 10; i++){
             Integer precio = random.nextInt(1000000);
             String marca = marcas[random.nextInt(marcas.length)];
             String color = colores[random.nextInt(colores.length)];
             Boolean elec = random.nextBoolean();
-            cocinas[i] = new Cocina("Modelo" + i, precio, marca, color, elec);
+            cocinas[i] = new Cocina("Modelo " + i, precio, marca, color, elec);
         }
         System.out.println("Cocinas recomendadas: ");
         for (Integer i = 0; i < 3; i++){
-            
+            System.out.println(cocinas[random.nextInt(cocinas.length)].toString());
+        }
+        System.out.println("----------");
+        System.out.println("Heladeras recomendadas: ");
+        for (Integer i = 0; i < 3; i++){
+            System.out.println(heladeras[random.nextInt(heladeras.length)].toString());
+        }
+        System.out.println("----------");
+        System.out.println("Lavarropas recomendados: ");
+        for (Integer i = 0; i < 3; i++){
+            System.out.println(lavarropas[random.nextInt(lavarropas.length)].toString());
+        }
+        System.out.println("----------");
+        System.out.println("Electrodomésticos recomendados: ");
+        for (Integer i = 0; i < 3; i++){
+            if (random.nextInt(4) == 1){
+                System.out.println(cocinas[random.nextInt(cocinas.length)].toString());
+            } else {
+                if (random.nextInt(4) == 2){
+                    System.out.println(heladeras[random.nextInt(heladeras.length)].toString());
+                } else {
+                    System.out.println(lavarropas[random.nextInt(lavarropas.length)].toString());
+                }
+            }
         }
     }
 }
