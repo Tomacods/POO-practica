8aa435b04b1f0777adf794f0d00f2268f92c967a
Mora <mor4lina2003@gmail.com>
2024-09-11 15:10:40 -0300

    mora

--- Practica Parcial/mora/recu/Persona.java
+++ Practica Parcial/mora/recu/Persona.java
@@ -1,3 +1,29 @@
-public class Persona {
+public abstract class Persona {
+    protected String nombre;
+    protected String apellido;
+    protected String dni;
+
+    public Persona (String nombre, String apellido, String dni){
+        this.nombre=nombre;
+        this.apellido=apellido;
+        this.dni=dni;
+    }
+    
+    public String getDni (){
+        return dni;
+    }
+    public String getApellido (){
+        return apellido;
+    }
+    public String getNombre(){
+        return nombre;
+    }
+
+    @Override
+    public String toString() {
+        return " Nombre:" + nombre + "\n" + "Apellido:" + apellido + "\n"+"Dni:" + dni + "\n";
+    }
+
+    
     
 }
