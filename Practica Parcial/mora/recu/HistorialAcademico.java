8db9e1a18bdf2f30218a84ef82cd32093681d129
Mora <mor4lina2003@gmail.com>
2024-09-11 16:27:59 -0300

    mora

--- Practica Parcial/mora/recu/HistorialAcademico.java
+++ Practica Parcial/mora/recu/HistorialAcademico.java
@@ -5,19 +5,22 @@ public class HistorialAcademico {
     /*Un historial académico de un alumno debe poder recibir una materia y una nota para asignarla
 a su hashmap. Además, debe poseer un método para imprimir todo el historial académico del
  alumno. */
-
-
-    public void ingresarNota(Materia materiaIngresada, Integer nota) {
-    if (materias.containsKey(materiaIngresada)) {
-        System.out.println("La materia ya está en el historial con su nota.");
-    }   else {
-        System.out.println("Añadiendo materia y nota al historial.");
-        materias.put(materiaIngresada, nota);
-            }
+public void ingresarNota(Materia materiaIngresada, Integer nota) {
+    if (materiaIngresada == null || nota == null) {
+       // System.out.println("Materia o nota inválida.");
+    return;
     }
-
+    if (materias.containsKey(materiaIngresada)) {
+       // System.out.println("La materia ya está en el historial.");
+    return;
+    }
+        else {
+        materias.put(materiaIngresada, nota);
+       // System.out.println("Materia y nota añadidas al historial.");
+    }
+}
     public void imprimirHistorial() {
-        System.out.println("Historial Academico:");
+       // System.out.println("Historial Academico:");
         for (Materia materia : materias.keySet()) /*keySet obtiene todas las materias que están guardadas como claves en el HashMap materias*/ {
             System.out.println("Materia: " + materia.getNombre_materia() + ", Nota: " + materias.get(materia)); //materias.get(materia) devuelve el valor asociado a la clave materia en el HashMap
         }
