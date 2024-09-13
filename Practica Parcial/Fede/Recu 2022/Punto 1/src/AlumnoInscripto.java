171ef7cd4cfe31a7d92b30b461f2b4e44bd76f76
federxldan <federoldan229@gmail.com>
2024-09-13 20:55:18 -0300

    ??????????

---
+++ Practica Parcial/Fede/Recu 2022/Punto 1/src/AlumnoInscripto.java
@@ -0,0 +1,6 @@
+public class AlumnoInscripto extends Exception{
+    
+    public AlumnoInscripto(String message){
+        super(message);
+    }
+}
--- Practica Parcial/Fede/Recu 2022/Punto 1/src/Materia.java
+++ Practica Parcial/Fede/Recu 2022/Punto 1/src/Materia.java
@@ -3,24 +3,58 @@ import java.util.ArrayList;
 public class Materia {
     
     private String nombre;
-
     private Integer codigo;
     private Integer año;
     private Profesor profesor;
-    private ArrayList<Alumno> alumnos = new ArrayList<>();
+    private ArrayList<Alumno> alumnos;
 
-    public Materia(String nombre, Integer codigo, Integer año){
+    public Materia(String nombre, Integer codigo, Integer año, Profesor profesor){
         this.nombre = nombre;
         this.codigo = codigo;
         this.año =año;
+        this.alumnos = new ArrayList<>();
     }
 
     public void addAlumnos(Alumno alumno){
         this.alumnos.add(alumno);
     }
 
-    public void agregarProfesor(Profesor profe){
-        this.profesor = profe;
+    public void imprimirAlumnos(){
+        for (Alumno alu:alumnos){
+            System.out.println(alu);
+        }
+    }
+
+    public String inscribirAlumno(Alumno alumno){
+        try {
+            if (alumnos.contains(alumno)){
+                throw new AlumnoInscripto("El alumno ya está inscripto en la materia.");
+            }
+            alumnos.add(alumno);
+            return "El alumno ha sido inscripto con éxito.";
+        } catch (AlumnoInscripto e){
+            return e.getMessage();
+        }
+    }
+
+    public String eliminarAlumno(Alumno alumno){
+        try {
+            if (!alumnos.contains(alumno)){
+                throw new AlumnoInscripto("El alumno no está inscripto en la materia.");
+            }
+            alumnos.remove(alumno);
+            return "El alumno ha sido eliminado con exito";
+        } catch (AlumnoInscripto e){
+            return e.getMessage();
+        }
+    }
+
+    public void setProfesor(Profesor profesor){
+        this.profesor = profesor;
+    }
+
+    public Profesor getProfesor(){
+        return profesor;
     }
 
     public String getNombre() {
@@ -31,14 +65,7 @@ public class Materia {
         this.nombre = nombre;
     }
 
-    public void imprimirAlumnos(){
-        for (Alumno alu:alumnos){
-            System.out.println(alu);
-        }
-    }
-
     public String toString() {
         return "Nombre: " + nombre + "\n" + "Código: " + codigo + "\n" + "Año: " + año + "\n" + "Profesor: " + profesor + "\n" + "Alumnos: " + alumnos;
     }
-
 }
