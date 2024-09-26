5050f59e9e7a2d1524d1e45f2d1e0cde0c30f8be
Cesarmi99 <cesarmiuni@gmail.com>
2024-09-26 14:42:31 -0300

    César

--- TP4/JAVA/Ejercicio 1/César/Punto1/src/App.java
+++ TP4/JAVA/Ejercicio 1/César/Punto1/src/App.java
@@ -2,6 +2,13 @@ import javax.swing.JOptionPane;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        int Boton 
+        int boton = JOptionPane.showConfirmDialog(null, "¿Está seguro que quiere dar de  baja al usuario?","Confirmación", JOptionPane.YES_NO_OPTION);
+        if (boton == JOptionPane.YES_OPTION){
+            JOptionPane.showMessageDialog(null,"El usuario ha sido dado de baja con exito.");
+        }else {
+            JOptionPane.showMessageDialog(null,"Se cancelo la acción.");
+        }
+        String text = JOptionPane.showInputDialog("Ingresa algo kpo");
+        JOptionPane.showMessageDialog(null, "Ingresaste eto kpo : "+text);
     }
 }
