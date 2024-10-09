8389b4c05ce5e9a5cc62b689724b263fda13e0c4
Tomas <Tomasds15@gmail.com>
2024-10-09 17:54:50 -0300

    Tomas

---
+++ TP5/Ejercicio 10/ObserverPatern/src/SistemaMeteorologico.java
@@ -0,0 +1,9 @@
+public class SistemaMeteorologico implements Observable{
+
+    private String EstadoClima;
+
+    public SistemaMeteorologico(String EstadoClima) {
+        this.EstadoClima = EstadoClima;
+    }
+    
+}
