3d0609c91a191ccfe43f5c40a7b7d0f530fe85a5
Tomas <Tomasds15@gmail.com>
2024-10-15 18:48:33 -0300

    Tomas

---
+++ Base de datos ejercicios/Tp6/Ejercicio2.sql
@@ -0,0 +1,6 @@
+-- 2. Crear un stored procedure llamado ‘sp_crear_base_turnos’, el cual cree la
+-- estructura de datos del modelo mediante lenguaje DDL.
+
+procedure sp_crear_base_turnos
+AS
+BEGIN
---
+++ Base de datos ejercicios/Tp6/Ejercicio3.sql
@@ -0,0 +1,6 @@
+-- 3. Crear los stored procedure básicos que permitan BORRAR, AGREGAR Y
+-- MODIFICAR los campos de una tabla con lenguaje DDL, indicando los parámetros
+-- necesarios, y en caso de que no se pueda realizar la consulta de edición, muestre
+-- un texto explicativo.
+
+create procedure sp_borrar_campo 
