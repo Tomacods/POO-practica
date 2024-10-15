dbabf1786431cae93158db619596742e4920bc4d
Tomas <Tomasds15@gmail.com>
2024-10-15 17:48:37 -0300

    Tomas

---
+++ Base de datos ejercicios/Tp6/Ejercicio1.sql
@@ -0,0 +1,9 @@
+create procedure sp_nombre_fecha_nacimiento
+AS
+BEGIN
+    SELECT nombre_persona, fecha_nacimiento
+    FROM personas;
+END;
+-- el procedimiento almacenado sp_nombre_fecha_nacimiento devuelve el nombre y la fecha de nacimiento de todas las personas en la tabla personas
+
+exec sp_nombre_fecha_nacimiento --ejecuta el procedimiento almacenado sp_nombre_fecha_nacimiento
