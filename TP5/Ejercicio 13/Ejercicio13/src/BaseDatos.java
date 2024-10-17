ad37a23e5a46fd3deafa1bfdb273972cf9bed690
Tomas <Tomasds15@gmail.com>
2024-10-16 21:29:05 -0300

    to

--- TP5/Ejercicio 13/Ejercicio13/src/BaseDatos.java
+++ TP5/Ejercicio 13/Ejercicio13/src/BaseDatos.java
@@ -1,22 +1,22 @@
 public class BaseDatos {
-        private static BaseDatos instance;
-    
-        private BaseDatos() {
-        }
-    
-        public static synchronized BaseDatos getInstance() {
-            if (instance == null) {
-                instance = new BaseDatos();
-            }
-            return instance;
-        }
-    
-        public void saveData(String data) {
-    
-        }
-    
-        public String getData(String query) {
-            return "data";
-        }
+    private static BaseDatos instance;
+
+    private BaseDatos() {
     }
-    
+
+    public static synchronized BaseDatos getInstance() {
+        if (instance == null) {
+            instance = new BaseDatos();
+        }
+        return instance;
+    }
+
+    public void guardarDatos(String datos) {
+        System.out.println("Datos guardados: " + datos);
+    }
+
+    public String recuperarDatos(String consulta) {
+        System.out.println("Consulta realizada: " + consulta);
+        return "Datos recuperados para: " + consulta;
+    }
+}
