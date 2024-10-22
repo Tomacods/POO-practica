170be768291fdf06881e91d529f1cf505fb1740f
federxldan <federoldan229@gmail.com>
2024-10-22 14:55:16 -0300

    EAAAAAAAAAAAA

--- TP6/Ejercicio1/fedep3/src/App.java
+++ TP6/Ejercicio1/fedep3/src/App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        for (Integer i=1; i <= 6; i++) {
+            HolaMundo thread = new HolaMundo(i);
+            thread.start();
+        }
     }
 }
